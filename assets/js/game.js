703519eeeb342460a38a1b9d09779d0427869dd3
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-03 18:11:45 -0700

    javascript new concepts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+function fight() {
+    console.log(playerName);
+    // this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+  }
+ fight();
