b161b26b9ddfa30fca740690e70b4893a3b65010
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-04 21:52:16 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,8 +32,10 @@ var fight = function(enemyName) {
         }
       }
   
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -52,7 +54,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -81,7 +84,7 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40,60);
       fight(pickedEnemyName);
       // if we're not at the last enemy in the array
      if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -133,7 +136,7 @@ var shop = function() {
 
     // increase health and decrease money
     playerHealth = playerHealth + 20;
-    playerMoney = playerMoney - 7;
+    playerMoney = Math.max(0, playerMoney - 10);
   }
   else {
     window.alert("You don't have enough money!");
@@ -147,7 +150,7 @@ case "upgrade":
 
    // increase attack and decrease money
     playerAttack = playerAttack + 6;
-    playerMoney = playerMoney - 7;
+    playerMoney = Math.max(0, playerMoney - 7);
   }
   else {
     window.alert("You don't have enough money!");
@@ -167,6 +170,13 @@ case "upgrade":
       shop();
       break;
   }
+  // function to generate a random numeric value
+ 
+};
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 // start the game when the page loads
 startGame();
